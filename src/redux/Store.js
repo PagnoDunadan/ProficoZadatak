f25ceec9b7b55011c1c44fcb2201a1b73ceede25
PagnoDunadan <pagnodunadan@gmail.com>
2017-12-12 11:17:54 +0100

    small refactoring

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,5 +1,11 @@
-import { createStore } from 'redux';
-import itemsApp from './CombineReducers.js';
+import { combineReducers, createStore } from 'redux';
+import searchString from './reducers/SearchString.js';
+import items from './reducers/Items.js';
+
+const itemsApp = combineReducers({
+  searchString,
+  items
+});
 
 const store = createStore(itemsApp);
 
