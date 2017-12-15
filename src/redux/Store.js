36af33b5be3c5be7fd32f3a5d1bee929a82db243
PagnoDunadan <pagnodunadan@gmail.com>
2017-12-15 15:50:18 +0100

    Removed axios. Code is a mess

--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,7 +1,6 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import searchString from './reducers/SearchString.js';
 import items from './reducers/Items.js';
-//import axios from 'axios';
 import thunk from "redux-thunk";
 
 const itemsApp = combineReducers({
@@ -12,37 +11,6 @@ const itemsApp = combineReducers({
 const middleware = applyMiddleware(thunk)
 
 const store = createStore(itemsApp, middleware);
-
-// axios.delete('http://192.168.1.10:3001/items/3')
-//   .then((response) => {
-//     console.log(response);
-//   });
-
-// axios.post('http://192.168.1.10:3001/items', {
-//     name: 'Name',
-//     category: 'Category',
-//     status: 'Status',
-//     manufacturer: 'Manufacturer',
-//     location: 'Location'
-//   })
-//   .then((response) => {
-//     console.log(response);
-//   });
-
-// axios.get('http://192.168.1.10:3001/items')
-//   .then((response) => {
-//     for (let i = 0; i < response.data.length; i++) {
-//       store.dispatch({
-//         type: 'ADD_ITEM',
-//         id: response.data[i].rowid,
-//         name: response.data[i].name,
-//         category: response.data[i].category,
-//         status: response.data[i].status,
-//         manufacturer: response.data[i].manufacturer,
-//         location: response.data[i].location
-//       })
-//     }
-//   });
 
 
 // fetch('http://192.168.1.10:3001/items')
